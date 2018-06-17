0dbddaba69016421d1580fe5dc16e85242b2a1c7
Taha Iftekhar <taha_iftekhar@yahoo.com>
2018-06-17 20:54:09 +0500

    Customized chart more

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,10 +1,19 @@
+import _ from 'lodash';
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
 export default (props)=>{
+    function average(data){
+        return _.round(_.sum(data)/data.length);
+    }
     return(
-        <Sparklines height={120} width={180} data={props.data}>
-            <SparklinesLine color={props.color}/>
-        </Sparklines>
+        <div>
+            <Sparklines height={120} width={180} data={props.data}>
+                <SparklinesLine color={props.color}/>
+                <SparklinesReferenceLine type="avg"/>
+            </Sparklines>
+
+            <div>{average(props.data)} {props.unit}</div>
+        </div>
     );
 };
--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -13,9 +13,9 @@ class WeatherList extends Component{
         return (
             <tr key={name}>
                 <td>{name}</td>
-                <td><Chart data={temps} color="orange" /></td>
-                <td><Chart data={pressures} color="green" /></td>
-                <td><Chart data={humidities} color="black" /></td>
+                <td><Chart data={temps} color="orange" unit="K" /></td>
+                <td><Chart data={pressures} color="green" unit="hPa" /></td>
+                <td><Chart data={humidities} color="black" unit="%" /></td>
             </tr>
         );
 
@@ -27,9 +27,9 @@ class WeatherList extends Component{
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
