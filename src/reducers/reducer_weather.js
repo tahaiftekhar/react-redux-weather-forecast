5aa562f50c0d8ad1d485644aa63fa94dfd500106
Taha Iftekhar <taha_iftekhar@yahoo.com>
2018-06-18 01:08:52 +0500

    If already exists. Don't add city in state

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -7,8 +7,19 @@ export default function(state=[], action){
 
     switch (action.type){
         case FETCH_WEATHER:
-            return [action.payload.data, ...state];
-            //Same as return state.concat([action.payload.data]);
+            var found = false;
+            debugger
+            for(var i=0;i<state.length;i++){
+                if(state[i].city.id == action.payload.data.city.id){
+                    found = true;
+                    break
+                }
+            }
+            if(!found)
+                return [action.payload.data, ...state];
+                //Same as return state.concat([action.payload.data]);
+            else
+                return state;
     }
     return state;
 }
